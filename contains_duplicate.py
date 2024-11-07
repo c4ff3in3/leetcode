9e58f9e5b4665f6e9453f04cdd92f4284fc15d6e
Jacob Ogle <jacobogle@proton.me>
2024-11-07 17:18:45 -0500

    contains_duplicate_217

---
+++ contains_duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # HashTable O(n) complexity
+        mp = {
+            # num_in_list : # count
+        }
+        # Runtime: O(n)
+        for i in nums:
+            if i not in mp.keys():
+                mp[i] = 1
+            else:
+                return True
+        
+        return False
